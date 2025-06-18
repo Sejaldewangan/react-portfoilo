5b7fb79c982f5ea21d06137e129d4b973dcb573d
sejal dewangan <shubham.dewangan.3440@gmail.com>
2025-06-18 16:18:54 +0530

    complete

--- src/pages/miniComponents/About.jsx
+++ src/pages/miniComponents/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
         <h1
           className="flex gap-4 items-center text-[2rem] sm:text-[2.75rem] 
           md:text-[3rem] lg:text-[3.8rem] leading-[56px] md:leading-[67px] 
-          lg:leading-[90px] tracking-[15px] mx-auto w-fit font-extrabold about-h1"
+          lg:leading-[90px] tracking-[15px] mx-auto w-fit font-[new] about-h1"
           style={{
             background: "hsl(222.2 84% 4.9%)",
           }}
         >
-          ABOUT <span className="text-tubeLight-effect font-extrabold">ME</span>
+          ABOUT <span className="text-tubeLight-effect font-[new]">ME</span>
         </h1>
         <span className="absolute w-full h-1 top-7 sm:top-7 md:top-8 lg:top-11 z-[-1] bg-slate-200"></span>
       </div>
@@ -24,11 +24,11 @@ const About = () => {
       <div>
         <div className="grid md:grid-cols-2 my-8 sm:my-20 gap-14">
           <div className="flex justify-center items-center">
-            {/* <img
-              src="/me.jpg"
+            <img
+              src="/my photo.jpg"
               alt="avatar"
-              className="bg-white p-2 sm:p-4 rotate-[25deg] h-[240px] sm:h-[340px] md:h-[350px] lg:h-[450px]"
-            // /> */}
+              className="bg-white p-2 sm:p-4 h-[240px] sm:h-[340px] md:h-[350px] lg:h-[400px]"
+             />
           </div>
           <div className="flex justify-center flex-col tracking-[1px] text-xl gap-5">
             <p>
