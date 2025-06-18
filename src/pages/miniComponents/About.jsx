27f0794e3a329884a6b7561dd744855b1ccdc9c2
sejal dewangan <shubham.dewangan.3440@gmail.com>
2025-06-18 17:43:49 +0530

    mistake

--- src/pages/miniComponents/About.jsx
+++ src/pages/miniComponents/About.jsx
@@ -24,11 +24,11 @@ const About = () => {
       <div>
         <div className="grid md:grid-cols-2 my-8 sm:my-20 gap-14">
           <div className="flex justify-center items-center">
-            <img
+            {/* <img
               src="/my photo.jpg"
               alt="avatar"
               className="bg-white p-2 sm:p-4 h-[240px] sm:h-[340px] md:h-[350px] lg:h-[400px]"
-             />
+             /> */}
           </div>
           <div className="flex justify-center flex-col tracking-[1px] text-xl gap-5">
             <p>
