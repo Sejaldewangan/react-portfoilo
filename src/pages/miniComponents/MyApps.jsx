abcf509e12fb09512ef8a92fa617a2eed7e8d568
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-06-30 18:42:09 +0530

    animetion

--- src/pages/miniComponents/MyApps.jsx
+++ src/pages/miniComponents/MyApps.jsx
@@ -1,77 +1,74 @@
-import { Card } from "@/components/ui/card";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Pagination, Navigation } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-      import  { useRef } from 'react';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import 'swiper/css';
-// Import Swiper styles
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import { images } from "./image";
 
+const variants = {
+  enter: (direction) => ({
+    x: direction > 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction) => ({
+    zIndex: 0,
+    x: direction < 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+};
 
-// import required modules
-const MyApps = () => {
-  const [apps, setApps] = useState([]);
-  useEffect(() => {
-    const getMyApps = async () => {
-      const { data } = await axios.get(
-        "https://mern-stack-portfolio-backend-code.onrender.com/api/v1/softwareapplication/getall",
-        { withCredentials: true }
-      );
-      setApps(data.softwareApplications);
-    };
-    getMyApps();
-  }, []);
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => Math.abs(offset) * velocity;
+
+function MyApps() {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const imageIndex = wrap(0, images.length, page);
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
   return (
-    <div className="w-full flex flex-col gap-8 sm:gap-12">
-      <h1 className="text-tubeLight-effect text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] tracking-[15px] dancing_text mx-auto w-fit">
-        MY WEBSITES
-      </h1>
-<div>  <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img src="/public/Screenshot 2025-06-13 104620.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/public/Screenshot 2025-06-13 103841.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="/public/Screenshot 2025-06-13 104415.png" alt="" /></SwiperSlide>
-        
-      </Swiper>
-    </></div>
-{/* // Import Swiper React components */}
+    <div className=" flex flex-col justify-center items-center overflow-hidden relative w-[800px]  ">
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+        className="w-[100%] h-auto rounded-[10px]"
+          key={page}
+          src={images[imageIndex]}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
+      </AnimatePresence>
 
-
-
-
-
-      {/* <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {apps &&
-          apps.map((element) => {
-            return (
-              <Card className="h-fit p-7 flex flex-col justify-center items-center gap-3" key={element._id}>
-                <img
-                  src={element.svg && element.svg.url}
-                  alt="skill"
-                  className="h-12 sm:h-24 w-auto"
-                />
-                <p className="text-muted-foreground text-center">
-                  {element.name}
-                </p>
-              </Card>
-            );
-          })}
-      </div> */}
+      <div className=" absolute bottom-0  -translate-y-[50%] select-none  text-white p-5-10 rounded-[50%] right-10 bg-[rgba(0, 0, 0, 0.3)]" onClick={() => paginate(-1)}>
+        ‹
+      </div>
+      <div className=" absolute bottom-0  -translate-y-[50%] select-none  text-white p-5-10 rounded-[50%] left-10 bg-[rgba(0, 0, 0, 0.3)]" onClick={() => paginate(1)}>
+        ›
+      </div>
     </div>
   );
-};
- 
+}
 
 export default MyApps;
---
+++ src/pages/miniComponents/image.js
@@ -0,0 +1,3 @@
+export const images= ["/public/Screenshot 2025-06-13 104620.png",
+        "/public/Screenshot 2025-06-13 103841.png",
+        "/public/Screenshot 2025-06-13 104415.png"]
