a9894fb8bad2e7f2beb4933a4745dd423c5696f9
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-07-05 18:21:18 +0530

    S4

--- src/pages/miniComponents/image.js
+++ src/pages/miniComponents/image.js
@@ -1,6 +1,6 @@
 export const images = [
-  { src: "/public/Screenshot 2025-06-13 104620.png" },
-  { src: "/public/Screenshot 2025-06-13 103841.png" },
-  { src: "/public/Screenshot 2025-06-13 104415.png" },
-  { src: "/public/image.png" },
+  { src: "/public/Screenshot 2025-06-13 104620.JPG" },
+  { src: "/public/Screenshot 2025-06-13 103841.JPG" },
+  { src: "/public/Screenshot 2025-06-13 104415.JPG" },
+  { src: "/public/image.JPG" },
 ];
